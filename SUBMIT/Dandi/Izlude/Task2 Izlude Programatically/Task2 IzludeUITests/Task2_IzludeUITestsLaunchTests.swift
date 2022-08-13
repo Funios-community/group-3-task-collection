c308d73dc99eb134aad1f7204eb4fe43ee557ae4
dandisinaga <dandisinaga@icloud.com>
2022-08-13 20:04:45 +0700

    Final Part Task Izlude and Challenge

---
+++ SUBMIT/Dandi/Izlude/Task2 Izlude Programatically/Task2 IzludeUITests/Task2_IzludeUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Task2_IzludeUITestsLaunchTests.swift
+//  Task2 IzludeUITests
+//
+//  Created by Mochamad Dandi on 13/08/22.
+//
+
+import XCTest
+
+class Task2_IzludeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
